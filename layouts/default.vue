1d421bf30331020ff5d3f3d1cd1466baa067d558
giusscos <giu.cosenza08@gmail.com>
2025-06-04 11:21:42 +0200

    feat: add main page component with basic structure

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <header>
+            header
+        </header>
+
+        <main>
+            <slot />
+        </main>
+
+        <footer>
+            footer
+        </footer>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <section>
+        <h1>
+            main
+        </h1>
+    </section>
+</template>
